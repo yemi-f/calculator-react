1ffc5278a6b4226efc92ddd8c84bca95914ec91b
Yemi Fakorede <yemifakorede@gmail.com>
2020-07-29 15:44:09 -0400

    removed button focus outline and added cursor: pointer to all button divs

--- src/ButtonPanel.js
+++ src/ButtonPanel.js
@@ -13,26 +13,26 @@ const ButtonPanel = (props) => {
                 <div onClick={() => props.operatorBtnOnClick(props.divide)} className="w-25 operator-btn-div"> <OperatorButton operator={props.divide} id="divide" /></div>
             </div>
             <div className="row mx-auto">
-                <div onClick={() => props.numberBtnOnClick(7)} className="w-25"> <NumberButton num={7} /></div>
-                <div onClick={() => props.numberBtnOnClick(8)} className="w-25"> <NumberButton num={8} /></div>
-                <div onClick={() => props.numberBtnOnClick(9)} className="w-25"> <NumberButton num={9} /></div>
+                <div onClick={() => props.numberBtnOnClick(7)} className="w-25 number-btn-div"> <NumberButton num={7} /></div>
+                <div onClick={() => props.numberBtnOnClick(8)} className="w-25 number-btn-div"> <NumberButton num={8} /></div>
+                <div onClick={() => props.numberBtnOnClick(9)} className="w-25 number-btn-div"> <NumberButton num={9} /></div>
                 <div onClick={() => props.operatorBtnOnClick(props.multiply)} className="w-25 operator-btn-div"> <OperatorButton operator={props.multiply} id="multiply" /></div>
             </div>
             <div className="row mx-auto">
-                <div onClick={() => props.numberBtnOnClick(4)} className="w-25"> <NumberButton num={4} /></div>
-                <div onClick={() => props.numberBtnOnClick(5)} className="w-25"> <NumberButton num={5} /></div>
-                <div onClick={() => props.numberBtnOnClick(6)} className="w-25"> <NumberButton num={6} /></div>
+                <div onClick={() => props.numberBtnOnClick(4)} className="w-25 number-btn-div"> <NumberButton num={4} /></div>
+                <div onClick={() => props.numberBtnOnClick(5)} className="w-25 number-btn-div"> <NumberButton num={5} /></div>
+                <div onClick={() => props.numberBtnOnClick(6)} className="w-25 number-btn-div"> <NumberButton num={6} /></div>
                 <div onClick={() => props.operatorBtnOnClick(props.subtract)} className="w-25 operator-btn-div"><OperatorButton operator={props.subtract} id="subtract" /></div>
             </div>
             <div className="row mx-auto">
-                <div onClick={() => props.numberBtnOnClick(1)} className="w-25"> <NumberButton num={1} /></div>
-                <div onClick={() => props.numberBtnOnClick(2)} className="w-25"><NumberButton num={2} /></div>
-                <div onClick={() => props.numberBtnOnClick(3)} className="w-25"> <NumberButton num={3} /></div>
+                <div onClick={() => props.numberBtnOnClick(1)} className="w-25 number-btn-div"> <NumberButton num={1} /></div>
+                <div onClick={() => props.numberBtnOnClick(2)} className="w-25 number-btn-div"><NumberButton num={2} /></div>
+                <div onClick={() => props.numberBtnOnClick(3)} className="w-25 number-btn-div"> <NumberButton num={3} /></div>
                 <div onClick={() => props.operatorBtnOnClick(props.add)} className="w-25 operator-btn-div"><OperatorButton operator={props.add} id="add" /></div>
             </div>
             <div className="row mx-auto">
-                <div onClick={() => props.numberBtnOnClick(0)} className="w-50" id="btn-0-div"><NumberButton num={0} /></div>
-                <div onClick={() => props.preventMoreDecimalPoints()} className="w-25"> <NumberButton num={"."} /></div>
+                <div onClick={() => props.numberBtnOnClick(0)} className="w-50 number-btn-div" id="btn-0-div"><NumberButton num={0} /></div>
+                <div onClick={() => props.preventMoreDecimalPoints()} className="w-25 number-btn-div"> <NumberButton num={"."} /></div>
                 <div onClick={() => props.operatorBtnOnClick("")} className="w-25 operator-btn-div"><OperatorButton operator={props.equals} id="equals" /></div>
             </div>
         </div>
